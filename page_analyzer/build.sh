5e2c25822499e5da5b464c5e4d9fb6f90f099169
gitfilin <vy.filin@yandex.ru>
2025-02-22 19:48:34 +0400

    Fix gunicorn command for render.com

--- page_analyzer/build.sh
+++ page_analyzer/build.sh
@@ -2,4 +2,5 @@
 # скачиваем uv и запускаем команду установки зависимостей
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-make install 
+make install
+uv pip install gunicorn 
